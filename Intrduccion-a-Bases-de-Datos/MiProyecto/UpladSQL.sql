7a39be8da2581da8d583b4411e2d5b135c6004e4
José Carlos <josecarloscorona.c@gmail.com>
2020-02-13 20:45:08 -0600

    Añadir SQL para subir data al server del curso

---
+++ Intrduccion-a-Bases-de-Datos/MiProyecto/UpladSQL.sql
@@ -0,0 +1,3 @@
+USE JCarlosCorona
+CREATE TABLE IF NOT EXISTS ProductPerf (SKU VARCHAR(20) PRIMARY KEY, Producto VARCHAR(300), Ingresos FLOAT(30,2), ComprasUnicas INT, Cantidad INT, PrecioMedio FLOAT(30,2), Reembolso FLOAT(30,2), PorcentCarritoDetalle FLOAT(2,2), PorcentCompraDetalle FLOAT(2,2));
+LOAD DATA LOCAL INFILE "" INTO TABLE ProductPerf;
