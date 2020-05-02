89ea0556f3b2d5b8ee71b7993549ef4bd50c4154
JCarlosCorona <josecarloscorona@gmail.com>
2020-05-02 13:06:09 -0500

    Actualizar Módulo archivos

--- Modulo03.Programacion-con-Python-para-Data-Analysis/Sesion-03/archivos.py
+++ Modulo03.Programacion-con-Python-para-Data-Analysis/Sesion-03/archivos.py
@@ -1,5 +1,5 @@
 import os
-
+import time
 ## Modelo-Vista-Controlador (MVC)
 
 # Modelo: Obtiene datos
@@ -15,29 +15,33 @@ def obtiene_entradas(ruta):
     nombres = [os.path.join(ruta, n) for n in nombres]  # listas de comprension
 
     entradas = []
+    total = 0
     for n in nombres:
+        fecha = time.ctime(os.path.getmtime(n))
         if os.path.isfile(n):
             #-obtener el tamaño de n-
             #-agregarlo a la lista-
             tamanio = os.path.getsize(n)
-            entrada = [n, tamanio]
+            entrada = [n, tamanio, fecha]
             entradas.append(entrada)
+            total += tamanio
         else:
             # Entonces es una carpeta
             #-asignar tamaño igual cero-
             #-asignarlo a la lista-
-            entrada = [n, 0]
+            entrada = [n, 0, fecha]
             entradas.append(entrada)
     
-    return entradas
+    return entradas, total
 
 # Vista: Genera resultados para el usuario.
-def imprime_entradas(entradas):
+def imprime_entradas(entradas, total):
     """ 
     Imprime la lista de las entradas en la salida estándar en forma tabular.
     """
     for e in entradas:
-        print("{:60} {:10}".format(*e))
+        print("{:60} {:10} {:24}".format(*e))
+    print('Total de bytes: {}'.format(total))
 
 # Contralador: Manipular las funciones del Modelo y la Vista
 def main(): #Función principal
@@ -45,8 +49,8 @@ def main(): #Función principal
     Función principal del script
     """
     ruta = '/Users/josecarlos/Documents/Github/CursoBD/Modulo03.Programacion-con-Python-para-Data-Analysis/Sesion-03'
-    entradas = obtiene_entradas(ruta)
-    imprime_entradas(entradas)
+    entradas, total = obtiene_entradas(ruta)
+    imprime_entradas(entradas, total)
 
 if __name__ == "__main__": # el script es el programa principal?
     main()
