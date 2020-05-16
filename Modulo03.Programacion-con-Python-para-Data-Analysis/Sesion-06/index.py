aae5b6388f827ac2e5a657c7ed4e37e5cb94943f
JCarlosCorona <josecarloscorona@gmail.com>
2020-05-16 10:29:46 -0500

    Actualizar index.csv

--- Modulo03.Programacion-con-Python-para-Data-Analysis/Sesion-06/index.py
+++ Modulo03.Programacion-con-Python-para-Data-Analysis/Sesion-06/index.py
@@ -1,4 +1,6 @@
-from bottle import route, run
+from bottle import route, run, HTTPResponse
+import csv
+import io
 import lc
 
 @route('/hola')
@@ -28,6 +30,14 @@ def index():
     # Obtener lista de archivos
     carpeta = lc.Carpeta('.')
     carpeta.obtiene_entradas()
+    # Construir la lista de entradas de tipo dict()
+    entradas = [
+        {'nombre': e[0], 'tamanio':e[1], 'fecha': e[2]}
+        for e in carpeta.entradas
+    ]
+    # Integrar a lista de entradas al dict()
+    dictArchivos = {'Archivos':entradas}
+    return dictArchivos
 
     #Integrar lista con html
     tablaHTML = '<table>'
@@ -39,5 +49,33 @@ def index():
 
     # Devuelve el resultado
     return pagina.format(tablaHTML)
+
+@route('/csv')
+def apicsv():
+    """Atiende peticion GET /csv"""
+    # Obtener la list de archivos
+    carpeta = lc.Carpeta('.')
+    carpeta.obtiene_entradas()
+    # Agregando fila de encabezaods
+    encabezados = ['Nombre', 'Tamaño','Fecha']
+    entradas = [encabezados]
+    entradas += carpeta.entradas
+
+    # Construir la lista en un archivo CSV en memoria RAM
+    da = io.StringIO()
+    csv_writer = csv.writer(da)
+    csv_writer.writerows(carpeta.entradas)
+    # Reiniciando índice del archivo
+    da.seek(0)
+    # Personalizar encabezados de respuesta del HTTP
+    h = {}
+    h['content-type'] = 'text/csv'
+    h['Content-Disposition'] = "attachment;filename = archivos.csv"
+
+    return HTTPResponse(
+        body=da.read(),
+        status=200,
+        headers=h
+    )
 
 run(host = 'localhost', port = 8080, debug = True)
